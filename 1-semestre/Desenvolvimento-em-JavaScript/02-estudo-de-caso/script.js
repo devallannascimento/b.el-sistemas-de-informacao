19aedf925f13265bac1e97379f7782caeae64c46
Állan Santos <allancontatopessoal@gmail.com>
2023-08-03 13:12:17 -0300

    adição de atividade

--- 1-semestre/Desenvolvimento-em-JavaScript/02-estudo-de-caso/script.js
+++ 1-semestre/Desenvolvimento-em-JavaScript/02-estudo-de-caso/script.js
@@ -1,28 +1,28 @@
 // determina aprovados para a segunda etapa
-function primeiraEtapa(acertos_na_prova, 
-    semestres_cursados){
-/*   
-* Help: 
-* acertos_na_prova recebe a quantidade de acertos na prova
-* semestres_cursados: semestres cursados
-*/
-// regras para aprovação
-const total_de_questoes = 20 
-const nota_minima_aprovacao = 0.7 
-const minimo_semestres = 3
+function primeiraEtapa(acertos_na_prova,
+    semestres_cursados) {
+    /*   
+    * Help: 
+    * acertos_na_prova recebe a quantidade de acertos na prova
+    * semestres_cursados: semestres cursados
+    */
+    // regras para aprovação
+    const total_de_questoes = 20
+    const nota_minima_aprovacao = 0.7
+    const minimo_semestres = 3
 
-// calcula a nota %
-let nota = acertos_na_prova / total_de_questoes
+    // calcula a nota %
+    let nota = acertos_na_prova / total_de_questoes
 
-// calcula se vai ser aprovado ou não
-if((nota >= nota_minima_aprovacao) && 
-(semestres_cursados >= minimo_semestres)){
-return "Aprovado";
-}
-else if(nota >= nota_minima_aprovacao){
-
-}
-else{
-return "Reprovado";
-}
+    // calcula se vai ser aprovado ou não
+    if ((nota >= nota_minima_aprovacao) &&
+        (semestres_cursados >= minimo_semestres)) {
+        return "Aprovado";
+    }
+    else if (nota >= nota_minima_aprovacao) {
+        return "Você foi incluído em nosso banco de currículos";
+    }
+    else {
+        return "Reprovado";
+    }
 }
